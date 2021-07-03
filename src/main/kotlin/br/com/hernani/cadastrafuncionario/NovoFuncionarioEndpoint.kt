6cbfebd14b04d5f28e242ae97e632f857b81edda
2020nani <nani_almeida23@hotmail.com>
2021-07-03 18:28:29 -0300

    adicionando logger na requisicao

--- src/main/kotlin/br/com/hernani/cadastrafuncionario/NovoFuncionarioEndpoint.kt
+++ src/main/kotlin/br/com/hernani/cadastrafuncionario/NovoFuncionarioEndpoint.kt
@@ -4,6 +4,7 @@ import br.com.hernani.FuncionarioRequest
 import br.com.hernani.FuncionarioResponse
 import br.com.hernani.FuncionarioServiceGrpc
 import io.grpc.stub.StreamObserver
+import org.slf4j.LoggerFactory
 import java.lang.IllegalStateException
 import javax.inject.Singleton
 
@@ -11,10 +12,13 @@ import javax.inject.Singleton
 class NovoFuncionarioEndpoint(
     val funcionarioRepository: FuncionarioRepository
 ) : FuncionarioServiceGrpc.FuncionarioServiceImplBase() {
+    //instancia logger
+    val logger = LoggerFactory.getLogger(NovoFuncionarioEndpoint::class.java)
     override fun cadastraFuncionario(
         request: FuncionarioRequest,
         responseObserver: StreamObserver<FuncionarioResponse>
     ) {
+        logger.info("Recebendo os dados de request funcionario ${request.nome}")
         val funcionario = request.converte()
 
         funcionarioRepository.save(funcionario)
@@ -23,6 +27,8 @@ class NovoFuncionarioEndpoint(
             .newBuilder()
             .setId(funcionario.id ?: throw IllegalStateException("Id nao pode ser nulo"))
             .build()
+
+        logger.info("Funcionario salvo com Id: ${response.id}")
         responseObserver.onNext(response)
         responseObserver.onCompleted()
     }

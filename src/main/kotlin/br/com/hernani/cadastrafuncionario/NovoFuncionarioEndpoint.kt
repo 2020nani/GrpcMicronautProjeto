921ab530f69e083d58acd8e78f9002a9206dd352
2020nani <nani_almeida23@hotmail.com>
2021-07-04 23:35:53 -0300

    adicionando exceptions

--- src/main/kotlin/br/com/hernani/cadastrafuncionario/NovoFuncionarioEndpoint.kt
+++ src/main/kotlin/br/com/hernani/cadastrafuncionario/NovoFuncionarioEndpoint.kt
@@ -1,8 +1,14 @@
 package br.com.hernani.cadastrafuncionario
 
+import br.com.hernani.ErrorDetails
 import br.com.hernani.FuncionarioRequest
 import br.com.hernani.FuncionarioResponse
 import br.com.hernani.FuncionarioServiceGrpc
+import br.com.hernani.validationcustomizadas.isCPF
+import com.google.protobuf.Any
+import com.google.rpc.Code
+import io.grpc.Status
+import io.grpc.protobuf.StatusProto
 import io.grpc.stub.StreamObserver
 import org.slf4j.LoggerFactory
 import java.lang.IllegalStateException
@@ -18,10 +24,53 @@ class NovoFuncionarioEndpoint(
         request: FuncionarioRequest,
         responseObserver: StreamObserver<FuncionarioResponse>
     ) {
+        // simulando verificacao seguranca
+        if (request.cpf.startsWith("336")){
+            val statusProto = com.google.rpc.Status.newBuilder()
+                .setCode(Code.PERMISSION_DENIED.number)
+                .setMessage("Usuario nao pode acessar este recurso")
+                .addDetails(
+                    Any.pack(ErrorDetails.newBuilder()
+                    .setCode(401)
+                    .setMessage("Token inexpirado")
+                    .build())
+                )
+                .build()
+            val e = StatusProto.toStatusRuntimeException(statusProto)
+            responseObserver.onError(e)
+        }
+        //tratando erros
+        val nome = request.nome
+        if (nome == null || nome.isBlank()) {
+            logger.info("Dados request invalido")
+            val error = Status.INVALID_ARGUMENT
+                .withDescription("Campo nome e obrigatorio")
+                .asRuntimeException()
+            responseObserver.onError(error)
+        }
+        val cpf = request.cpf
+        if (!isCPF(cpf) || cpf == null || cpf.isBlank()) {
+            logger.info("Cpf invalido")
+            val error = Status.INVALID_ARGUMENT
+                .withDescription("Campo cpf esta invalido\n")
+                .augmentDescription("Formato cpf deve ser xxxxxxxxxxx e ser valido")
+                .asRuntimeException()
+            responseObserver.onError(error)
+        }
         logger.info("Recebendo os dados de request funcionario ${request.nome}")
+
         val funcionario = request.converte()
 
-        funcionarioRepository.save(funcionario)
+        try {
+            funcionarioRepository.save(funcionario)
+        } catch (e: Exception) {
+            responseObserver.onError(
+                Status.INTERNAL
+                    .withDescription(e.message)
+                    .withCause(e) //anexado status de erro porem nao e enviado ao client
+                    .asRuntimeException()
+            )
+        }
 
         val response = FuncionarioResponse
             .newBuilder()
